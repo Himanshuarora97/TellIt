c7bca5debda33cd28bddbd0360aebc78bc4a94b2
himanshu <iamhimanshuarora97@gmail.com>
2017-03-28 16:35:56 +0530

    remove Butterknife errors

--- app/src/main/java/com/example/makroid/tellit/chosenStory.java
+++ app/src/main/java/com/example/makroid/tellit/chosenStory.java
@@ -10,69 +10,74 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
-/**
- * Created by joginderpal on 27-03-2017.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class chosenStory extends AppCompatActivity {
 
-  //  @BindView(R.id.app_bar_story)        //Gives Error
+    private static final String TAG = chosenStory.class.getClass().getName();
+    @BindView(R.id.app_bar_story)
     AppBarLayout appBarLayout;
-   // @BindView(R.id.toolbar_layout)          //Gives Error
+    @BindView(R.id.toolbar_layout)
     CollapsingToolbarLayout collapsingToolbarLayout;
-   // @BindView(R.id.toolbar_story)        //Gives Error
+    @BindView(R.id.toolbar_story)
     Toolbar toolbar;
-
+    @BindView(R.id.rvrank)
     RecyclerView recyclerView;
+    @BindView(R.id.toolImage)
+    ImageView toolImageView;
+
+    // RecyclerView variables
     RecyclerView.LayoutManager layoutManager;
     RecyclerView.Adapter adapter;
 
+    //For unbind Butterknife
+    Unbinder unbinder;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chosen_story);
-        recyclerView = (RecyclerView) findViewById(R.id.rvrank);
-        appBarLayout= (AppBarLayout) findViewById(R.id.app_bar_story);
-        collapsingToolbarLayout= (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
-        toolbar= (Toolbar) findViewById(R.id.toolbar_story);
+        unbinder = ButterKnife.bind(this);
         setSupportActionBar(toolbar);
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
 
-            boolean a=true;
-            int scroll=-1;
+//            boolean a=true;
+//            int scroll=-1;
 
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+//               if (scroll==-1){
+//
+//                   scroll=appBarLayout.getTotalScrollRange();
+//               }
 
-               if (scroll==-1){
-
-                   scroll=appBarLayout.getTotalScrollRange();
-               }
-
-                else if (scroll+verticalOffset==0){
-                   collapsingToolbarLayout.setTitle("Story Title");
+                if (verticalOffset==0){
+                   collapsingToolbarLayout.setTitle("Story Title Here");
                    collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
                    collapsingToolbarLayout.setCollapsedTitleTypeface(Typeface.defaultFromStyle(Typeface.BOLD_ITALIC));
-                   a=true;
                }
-
-                else if (a){
-
-                   TranslateAnimation translateAnimation=new TranslateAnimation(-200,0,0,0);
-                  // Animation a1=new AlphaAnimation(0,1);
-                  // a1.setDuration(500);
-                   translateAnimation.setDuration(500);
-                   AnimationSet anim=new AnimationSet(true);
-                   anim.addAnimation(translateAnimation);
-                   collapsingToolbarLayout.startAnimation(anim);
-                   collapsingToolbarLayout.setTitle(" ");
-
-                    a=false;
-               }
+//
+//                else if (a){
+//
+////                   TranslateAnimation translateAnimation=new TranslateAnimation(-200,0,0,0);
+////                  // Animation a1=new AlphaAnimation(0,1);
+////                  // a1.setDuration(500);
+////                   translateAnimation.setDuration(500);
+////                   AnimationSet anim=new AnimationSet(true);
+////                   anim.addAnimation(translateAnimation);
+////                   collapsingToolbarLayout.startAnimation(anim);
+//                   collapsingToolbarLayout.setTitle(" ");
+//
+//                    a=false;
+//               }
 
 
             }
@@ -85,5 +90,11 @@ public class chosenStory extends AppCompatActivity {
         //   recyclerView.findViewHolderForAdapterPosition(45);
         recyclerView.setAdapter(adapter);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }
